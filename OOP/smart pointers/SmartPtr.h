e416cbcb33ffd205ece7b839a41d23b58a797aea
dragon4eg <dragon4eg@gmail.com>
2014-02-11 02:41:50 +0200

    reference counting
    mow not only one smart pointer can own an object
    Aleksandresku 187-188
    added valashko's reporter testing class

--- OOP/smart pointers/SmartPtr.h
+++ OOP/smart pointers/SmartPtr.h
@@ -1,43 +1,77 @@
 #pragma once
+using std::cout;
+using std::endl;
+
+class RefCounter
+{
+	private:
+		unsigned _count;
+	public:
+		void addRef()
+		{
+			++_count;
+		}
+		unsigned decRef()
+		{
+			return --_count;
+		}
+};
 
 template <typename T> class SmartPtr
 {
-	private:
+	private:		
 		T* _pointee;
+		RefCounter* _count;
 	public:
 		explicit SmartPtr(T* pointee = 0): _pointee(pointee)
 		{
+			_count=new RefCounter;
+			_count->addRef();
 			return;
 		}
-		SmartPtr(const SmartPtr<T>& smart): _pointee(smart._pointee) 
+		SmartPtr(SmartPtr<T>& smart): _pointee(smart._pointee) 
 		{
-			smart._pointee = 0;
+			_count->addRef();
 		}
 		~SmartPtr()
 		{
-			delete _pointee;
+			if(_count->decRef() == 0)
+			{
+				delete _pointee;
+				delete _count;
+			}
+			//_count->_count = _count->decRef();
 		}
-		SmartPtr<T>& operator=(const SmartPtr<T>& smart)
+		SmartPtr<T>& operator=(SmartPtr<T>& smart)
 		{
 			if (this == &smart)
 			{
 				return *this;
 			}
-			delete _pointee;
-			_pointee = smart._pointee;
-			smart._pointee = 0;
+			if (_count->decRef() == 0)
+			{
+				delete _pointee;
+				cout<<"The pointer you assign to was the last for his object. Old object deleted"<<endl;
+			}
+			_pointee = smart._pointee; 
+			smart._count->addRef();
+			_count = smart._count;
 			return *this;
 		}
-
-		T& operator*(); const //return the &object but not change it with this operator;
+		const T* showPointee() const
+		{
+			return _pointee;
 		}
+		T& operator*() const //return the &object but not change it with this operator;
+		{	
 			return *_pointee;
 		}
-		T* operator->() cosnt
+		T* operator->() const
 		{
 			return _pointee;
 		}
 };
+
 
 /*********************************** friend operators **************************************/
 /* == != < > >= <= we need just < and == for all others to implement; isNULL sort of...*/
